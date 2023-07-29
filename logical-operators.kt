2b7b8a222cae5f60ff40562e597bc774c633e888
umardar8 <umerfarooqdar.official@gmail.com>
2023-07-29 05:10:57 +0500

    added uses of AND, OR and NOT

--- logical-operators.kt
+++ logical-operators.kt
@@ -27,4 +27,37 @@ fun main() {
     println(10 <= 10) // true 
     println(20 <= 10) // false 
     println(10 <= 20) // true 
+
+    // use of AND operator: &&
+    print(true && true)  // true 
+    print(true && false)  // false 
+    print(false && true)  // false 
+    print(false && false)  // false
+
+    val finishedHomework = false // or true 
+    val cleanedRoom = true // or false 
+    val canPlayGames = finishedHomework && cleanedRoom 
+    println(canPlayGames)
+
+    // use of OR operator: ||
+    print(true || true)  // true 
+    print(true || false)  // true 
+    print(false || true)  // true 
+    print(false || false)  // false 
+
+    val carCleaned = false // or true 
+    val grassCut = true // or false 
+    val canGoToCinema = carCleaned || grassCut 
+    println(canGoToCinema)
+
+    // use of NOT operator: !
+    println(!true) // false 
+    println(!false) // true 
+
+    val isAmazing = true 
+    print(!isAmazing) // false 
+    
+    isBoring = false 
+    print(!isBoring) // true 
+    
 }
