a841b4d1a1d1d29c93f8fe2903724d8f11989c37
umardar8 <umerfarooqdar.official@gmail.com>
2023-08-05 00:18:33 +0500

    use of When conditional

---
+++ when-conditional.kt
@@ -0,0 +1,46 @@
+fun main() {
+    fun main() {
+        // use of "when" conditional in Kotlin
+        var password = "ABC"
+        
+        when {
+            password == "" -> {
+                println("password can not be empty.")
+            }
+            password.length < 7 -> {
+                println("password should contain atleast 7 characters")
+            }
+            else -> {
+                println("Nice password!")
+            }
+        }
+        
+        // the braces can be ommited if there is only one statement in branch
+        password = ""
+        when {
+            password == "" -> println("password can not be empty.")
+            password.length < 7 -> println("password should contain atleast 7 characters")
+            else -> println("Nice password!")
+        }
+        
+        // when can also be used as an expression to directly return some value
+        password = "ABCDQWERTY"
+        var textToShow = when {
+            password == "" -> "password can not be empty."
+            password.length < 7 -> "password should contain atleast 7 characters"
+            else -> "Nice password!"
+        }
+        println(textToShow)
+        
+        // if the condition to check is given once with when keyword in parenthesis
+        // you can use the when conditional as a Switch-Case
+        val number = 2
+        textToShow = when(number) {
+            1 -> "One"
+            2 -> "Two"
+            3 -> "Three"
+            else -> "Try Again!"
+        }
+        println(textToShow)
+    }
+}
